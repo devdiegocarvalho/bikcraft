78aebedf25c75257087054c20ed562fc26df5627
Diego Carvalho <dev.diegocarvalho@gmail.com>
2022-04-03 12:50:55 -0300

    js Galeria de imagens

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Ativar links do Menu
-
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -14,7 +13,6 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar itens do orçamento
-
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -27,7 +25,6 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 // Perguntas frequentes
-
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
@@ -45,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
